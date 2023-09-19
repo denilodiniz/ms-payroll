99823d27b78967eda7f5ee4c77669e5ea42757d6
Denilo Diniz <denilo_diniz@outlook.com>
2023-09-19 00:05:23 -0300

    Tests created for Worker Resource.

---
+++ hr-worker/src/test/java/br/com/ddev/hrworker/resources/WorkerResourceTest.java
@@ -0,0 +1,89 @@
+package br.com.ddev.hrworker.resources;
+
+import br.com.ddev.hrworker.dtos.WorkerDTO;
+import br.com.ddev.hrworker.entities.Worker;
+import br.com.ddev.hrworker.resources.exceptions.StandardError;
+import br.com.ddev.hrworker.servicies.WorkerService;
+import br.com.ddev.hrworker.servicies.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class WorkerResourceTest {
+
+    @InjectMocks
+    WorkerResource workerResource;
+
+    @Mock
+    WorkerService workerService;
+
+    private WorkerDTO workerDTO;
+    private StandardError standardError;
+
+    @BeforeEach
+    void setUp() {
+        workerDTO = new WorkerDTO(
+                new Worker(1L, "Bob", 200.0)
+        );
+        standardError = new StandardError(
+                HttpStatus.NOT_FOUND.value(),
+                "Resource not found.",
+                "Worker with id " + workerDTO.getId() + " does not exist.",
+               "localhost:8080/" + workerDTO.getId()
+        );
+    }
+
+    @Test
+    @DisplayName("Find all workers successfully")
+    void findAllWorkersSuccessfully() {
+        List<WorkerDTO> workerDTOList = new ArrayList<>();
+        workerDTOList.add(workerDTO);
+        when(workerService.findAll()).thenReturn(workerDTOList);
+
+        ResponseEntity<List<WorkerDTO>> result = workerResource.findAll();
+
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertEquals(workerDTOList, result.getBody());
+        assertEquals(workerDTO.getClass(), result.getBody().get(0).getClass());
+    }
+
+    @Test
+    @DisplayName("Find all workers with empty list")
+    void findAllWorkersWithEmptyList() {
+        List<WorkerDTO> workerDTOList = new ArrayList<>();
+
+        when(workerService.findAll()).thenReturn(workerDTOList);
+
+        ResponseEntity<List<WorkerDTO>> result = workerResource.findAll();
+
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertEquals(result.getBody().isEmpty(), true);
+    }
+
+    @Test
+    @DisplayName("Find worker by id successfully")
+    void findWorkerByIdSuccessfully() {
+        when(workerService.findById(Mockito.anyLong())).thenReturn(workerDTO);
+
+        ResponseEntity<WorkerDTO> result = workerResource.findById(Mockito.anyLong());
+
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertEquals(workerDTO, result.getBody());
+        assertEquals(workerDTO.getClass(), result.getBody().getClass());
+    }
+
+}
